d489f6c2e21a7869d048f5c226157faa71885f37
martin-rs <martin@m-rs.xyz>
2022-07-20 23:19:15 +0200

    update

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "~/.scripts/statusbar/sb-weather",	                                               300,	0},
+    {"", "sb-weather",	                                               300,	0},
     {"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                       30,	0},
-    {"", "~/.scripts/statusbar/sb-diskspace",                                                0,       0},
-    {"", "~/.scripts/statusbar/sb-nw_connections",                                           5,       0},
-    {"", "~/.scripts/statusbar/sb-battery",					               5,	0},
+    {"", "sb-diskspace",                                                0,       0},
+    {"", "sb-nw_connections",                                           5,       0},
+    {"", "sb-battery",					               5,	0},
     {"", "date '+%Y %b %d (%a) %R:%S'",					                       1,	0},
 };
 

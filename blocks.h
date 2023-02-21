734122d3f604df02952f70b615e6400774039085
mr <martin7273@protonmail.com>
2023-02-21 15:10:37 +0100

    noto emojies

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"", "sb-weather",	                                               300,	0},
-    {"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                       30,	0},
-    {"", "sb-diskspace",                                                0,       0},
+    {"🧮 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                       30,	0},
+    {"💾 ", "sb-diskspace",                                                0,       0},
     {"", "sb-nw_connections",                                           5,       0},
     {"", "sb-battery",					               5,	0},
-    {"", "date '+%Y %b %d (%a) %R:%S'",					                       1,	0},
+    {"📟 ", "date '+%Y %b %d (%a) %R:%S'",					                       1,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
